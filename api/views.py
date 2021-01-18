a508046777a099cbfc171cf59d7c322d559485a6
Himanshu <himanshudhiman9313@gmail.com>
2021-01-18 22:49:15 +0530

    updated

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from api import models
 import json
+import json
 import asyncio
 import os
 
@@ -12,8 +13,12 @@ STATE = 1
 
 @api_view(['POST'])
 def run(request):
-    STATE = 0
     data = request.POST
+    probId = data['problemId']
+    tempPath = os.path.join(BASE_DIR, 'TestCases', probId, 'main.json')
+    with open(tempPath) as f:
+        info = json.load(f)
+    totaltc = info['testcases']
     if data['lang'] == "CP":
         f = open(os.path.join(BASE_DIR, 'Codes/main.cpp'), "w")
         f.write(data['code'])
@@ -22,15 +27,34 @@ def run(request):
         f = open(os.path.join(BASE_DIR, 'Codes/main.py'), "w")
         f.write(data['code'])
         f.close()        
-
+    isInputGiven = False
     if('inp' in data.keys() and data['inp'] != None):
+        isInputGiven = True
         f = open(os.path.join(BASE_DIR, 'Codes/input.txt'), "w")
         f.write(data['inp'])
         f.close()
     os.chdir(os.path.join(BASE_DIR, 'Codes'))
     if data['lang'] == "CP":
         os.system('g++ "main.cpp"')
-        os.system('a.exe < input.txt > output.txt')
+        cnt = 0
+        if(isInputGiven == False):
+            for i in range(1, totaltc+1):
+                isSame = True
+                inpPath = os.path.join(BASE_DIR, 'TestCases', probId, 'input'+str(i)+'.txt')
+                os.system(f'./a.out < {inpPath} > output.txt')
+                with open(os.path.join(BASE_DIR, 'TestCases', probId, 'output'+str(i)+'.txt')) as f1, open('output.txt') as f2:
+                    for line1, line2 in zip(f1, f2):
+                        if line1 != line2:
+                            isSame = False
+                            break
+                if(isSame):
+                    cnt += 1
+            f = open(os.path.join(BASE_DIR, 'Codes/output.txt'), "r+")
+            f.truncate(0)
+            f.close()
+        else:
+            os.system('./a.out < input.txt > output.txt')
+        
         os.system('g++ "main.cpp" 2> "output.log"')
 
     if data['lang'] == "P3":
@@ -42,15 +66,14 @@ def run(request):
     f = open(os.path.join(BASE_DIR, 'Codes/input.txt'), "r+")
     f.truncate(0)
     f.close()
+    tcString = str(cnt) + "/" + str(totaltc)
     if os.stat(os.path.join(BASE_DIR, "Codes/output.log")).st_size != 0:
         f = open(os.path.join(BASE_DIR, "Codes/output.log"), "r")
         error = f.read()
-        models.Submission.objects.filter(pk = data['id']).update(error = error, status = "CE")
-        STATE = 1
+        models.Submission.objects.filter(pk = data['id']).update(error = error, status = "CE", testCasesPassed = tcString)
         return Response()
     else:
-        models.Submission.objects.filter(pk = data['id']).update(outputGen = code_output, status = "AC")
-        STATE = 1
+        models.Submission.objects.filter(pk = data['id']).update(outputGen = code_output, status = "AC", testCasesPassed = tcString)
         return Response()
 
 
